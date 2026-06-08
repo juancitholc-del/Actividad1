8ce7825a0c7bb495de05cd2564e01ef89648081a
juancitholc-del <juancitho.lc@gmail.com>
2026-06-07 20:35:08 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+Console.WriteLine("Calculo del Descuento de las Compras");
+Console.WriteLine("------------------------------------");
+
+Console.Write("Ingrese el monto de las compras:");
+double compra = double.Parse(Console.ReadLine());
+
+double desc, tpago;
+
+if (compra >= 100)
+{
+    desc = compra * 0.15;
+    Console.WriteLine("tiene un descuento de 15%");
+    tpago = compra - desc;
+    Console.WriteLine($"El total a pagar es: {tpago:F2}");
+}
+else
+{
+     desc = 0;
+    Console.WriteLine(" No cuenta con descuento");
+    tpago = compra - desc;
+    Console.WriteLine($"El total a pagar es: {tpago:F2}");
+}
+
+
